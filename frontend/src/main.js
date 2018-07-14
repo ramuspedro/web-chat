7dc80f8bc2d858790e630d965b7bcf284c2781c6
Pedro Ramos <ramuspedro@gmail.com>
2018-07-14 17:18:19 -0300

    working on router in vuejs

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
+import Router from 'vue-router';
+
 import App from './App.vue'
+import { routes } from "./router/index.js";
+
+Vue.use(Router);
+
+const router = new Router({
+  routes,
+  mode: 'history',
+});
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ frontend/src/router/index.js
@@ -0,0 +1,7 @@
+import Home from '@/components/Home';
+
+export const routes = [{
+  path: '/',
+  component: Home,
+  children: [],
+}, ];
