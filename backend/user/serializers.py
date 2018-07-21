532f7451dbf1cecb367c5d1effc34e0fd80379d2
Pedro Ramos <ramuspedro@gmail.com>
2018-07-20 21:39:28 -0300

    getting id of user

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -2,8 +2,15 @@
 
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        fields = ('id', 'username', 'email')
         model = get_user_model()
+        fields = ('id', 'username', 'email')
+
+class TokenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Token
+        fields = ('key', 'user')
