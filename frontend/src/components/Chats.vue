bf998c7e9c00db94448ae840f3b771a2f13d82da
Pedro Ramos <ramuspedro@gmail.com>
2018-07-25 23:02:03 -0300

    showing username chat room

--- frontend/src/components/Chats.vue
+++ frontend/src/components/Chats.vue
@@ -23,6 +23,7 @@
             alt="Card image cap">
           <div class="card-body">
             <h5 class="card-title">{{item.room_name}}</h5>
+            <h6>{{item.owner.username}}</h6>
             <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
             <a href="#" class="btn btn-primary">Go somewhere</a>
           </div>
